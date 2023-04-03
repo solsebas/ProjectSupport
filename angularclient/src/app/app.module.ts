ac13e250817dd5ac60706798332299bef822b04c
xVerTus <szympal720@student.polsl.pl>
2023-04-04 00:36:53 +0200

    Wtępny szkielet strony. Został ogarnięty wygenerowany kod oraz dodany component navbar który odpowiada za pasek nawigacji na calej stronie. Navbar został zaimplementowany w 40%.

--- angularclient/src/app/app.module.ts
+++ angularclient/src/app/app.module.ts
@@ -3,13 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FormsModule } from "@angular/forms";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {MDBBootstrapModule} from "angular-bootstrap-md";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MDBBootstrapModule.forRoot(),
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
