d92f424dd1c9f652105491eff3af5c0f6d155401
Marcel-Furs <72353903+Marcel-Furs@users.noreply.github.com>
2023-04-30 23:23:10 +0200

    Dodanie tematu wraz z supervisorem

--- src/main/java/pl/polsl/projectsupport/dao/SupervisorDao.java
+++ src/main/java/pl/polsl/projectsupport/dao/SupervisorDao.java
@@ -5,12 +5,9 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import pl.polsl.projectsupport.model.SupervisorModel;
-import pl.polsl.projectsupport.model.TopicModel;
-
-import java.math.BigInteger;
-import java.util.List;
 
 @Repository
-public interface TopicDao extends JpaRepository<TopicModel, Long> {
-
+public interface SupervisorDao extends JpaRepository<SupervisorModel, Long> {
+    @Query(value = "select * from supervisor s where s.user_id = :id" , nativeQuery = true)
+    public SupervisorModel findSupervisor(@Param("id") Long idUser);
 }
