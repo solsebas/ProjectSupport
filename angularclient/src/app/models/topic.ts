9d4db6ce1ffc13ebc0375c6289adbc81634b5efa
Sebastian Sołtysik <sebasol691@student.polsl.pl>
2023-05-07 16:33:08 +0200

    topic wysyłany z id

--- angularclient/src/app/models/topic.ts
+++ angularclient/src/app/models/topic.ts
@@ -1,10 +1,13 @@
 export class Topic {
+  id?: bigint
   topicName: string;
   topicDescription: string;
+
   idUser?: BigInt;
 
   constructor(topicName: string, topicDescription: string) {
     this.topicName = topicName;
     this.topicDescription = topicDescription;
   }
+
 }
