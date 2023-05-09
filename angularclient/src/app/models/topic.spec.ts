db27bea3ff400f52da5330c165998fb6638bfc89
Sebastian Sołtysik <sebasol691@student.polsl.pl>
2023-05-09 23:22:56 +0200

    poprawki po ciężkim mergde'u

--- angularclient/src/app/models/topic.spec.ts
+++ angularclient/src/app/models/topic.spec.ts
@@ -2,6 +2,6 @@ import { Topic } from './topic';
 
 describe('Topic', () => {
   it('should create an instance', () => {
-    expect(new Topic("","", 0)).toBeTruthy();
+    expect(new Topic("","")).toBeTruthy();
   });
 });
